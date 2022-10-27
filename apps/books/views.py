003dd5d8b66286d98507373bd407e3ea475b56f7
pg-7v <dmkseev@gmail.com>
2022-10-27 16:09:39 +0300

    feat: rename main folder, added migrations, add template html

---
+++ apps/books/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from apps.books.models import Book
+
+
+class BookListView(ListView):
+    model = Book
+    template = 'book_list.html'
